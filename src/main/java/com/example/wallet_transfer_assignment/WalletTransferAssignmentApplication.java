4e11e14f99582173b5a847b92553bf4ef4126764
Aditya Gupta <guptaaditya0611@gmail.com>
2026-06-16 22:13:40 +0530

    Add springboot setup

---
+++ src/main/java/com/example/wallet_transfer_assignment/WalletTransferAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallet_transfer_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTransferAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletTransferAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wallet_transfer_assignment/WalletTransferAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallet_transfer_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletTransferAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
